2273c41958b85e40d6d5d7e59a7783888e50f50e
Jonny Strömberg <jonny.stromberg@gmail.com>
2020-11-02 22:09:07 +0100

    Update deps and add redirect

--- src/pages/67.js
+++ src/pages/67.js
@@ -1,12 +1,15 @@
-import React from "react"
-import { Helmet } from "react-helmet"
+import React, { Component } from "react"
 
-const Meetup67 = () => (
-  <Helmet>
-    <script>
-      window.location.replace("https://event.on24.com/eventRegistration/EventLobbyServlet?target=reg30.jsp&referrer=https%3A%2F%2Fwcc.on24.com%2Fwebcast%2Fwebcasts&eventid=2821392&sessionid=1&key=E0878DAA4A3EA37F5350FA901D1DC392&regTag=&sourcepage=register")
-    </script>
-  </Helmet>
-)
+class Meetup67 extends Component {
+  componentDidMount() {
+    window.location.replace(
+      "https://event.on24.com/eventRegistration/EventLobbyServlet?target=reg30.jsp&referrer=https%3A%2F%2Fwcc.on24.com%2Fwebcast%2Fwebcasts&eventid=2821392&sessionid=1&key=E0878DAA4A3EA37F5350FA901D1DC392&regTag=&sourcepage=register"
+    )
+  }
+
+  render() {
+    return <div />
+  }
+}
 
 export default Meetup67
